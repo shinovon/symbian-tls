0923ba81c8e66a404769ac55910296deff089f50
Shinovon <arman200711@gmail.com>
2026-03-19 08:08:54 +0500

    Migrate to EPL 2.0

--- src/tlsconnection.cpp
+++ src/tlsconnection.cpp
@@ -2,9 +2,9 @@
 * Copyright (c) 2003-2009 Nokia Corporation and/or its subsidiary(-ies).
 * All rights reserved.
 * This component and the accompanying materials are made available
-* under the terms of "Eclipse Public License v1.0"
+* under the terms of "Eclipse Public License v2.0"
 * which accompanies this distribution, and is available
-* at the URL "http://www.eclipse.org/legal/epl-v10.html".
+* at the URL "http://www.eclipse.org/legal/epl-v20.html".
 */
 
 #include "tlsconnection.h"
